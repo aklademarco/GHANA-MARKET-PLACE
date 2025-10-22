fc7ab24d9f719b465f85c5ada45b0566493e5fb8
aklademarco <aklademarco2@gmail.com>
2025-10-22 12:13:13 +0000

    Enhance Navbar component with mobile menu and improve styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
 import { NavLink, Link } from "react-router";
-import { Smile, Search, ShoppingCart } from "lucide-react";
+import { Smile, Search, ShoppingCart, Menu, X } from "lucide-react";
+
 
 
 const Navbar = () => {
+  const [visible, setVisible] = useState(false);
   return (
-    <div className=" flex items-center justify-between py-5 font-sans">
+    <div className=" flex items-center justify-between py-5 font-sans relative">
       <img
         src={assets.logo}
         alt="Ghana Market Place Logo"
         className="h-12 w-auto"
       />
-      <ul className="hidden sm:flex gap-5 text-mdf text-gray-600">
-        <NavLink to="/" className="flex flex-col items-center gap1">
+      <ul className="hidden sm:flex gap-5 text-md text-gray-600">
+        <NavLink to="/" className="flex flex-col items-center gap-1">
           <p>HOME</p>
           <hr className=" w-2/4 border-none h-[1.5px] bg-gray-600 hidden" />
         </NavLink>
-        <NavLink to="/collection" className="flex flex-col items-center gap1">
+        <NavLink to="/collection" className="flex flex-col items-center gap-1">
           <p>COLLECTION</p>
           <hr className=" w-2/4 border-none h-[1.5px] bg-gray-600 hidden" />
         </NavLink>
-        <NavLink to="/about" className="flex flex-col items-center gap1">
+        <NavLink to="/about" className="flex flex-col items-center gap-1">
           <p>ABOUT</p>
           <hr className=" w-2/4 border-none h-[1.5px] bg-gray-600 hidden" />
         </NavLink>
-        <NavLink to="/contact" className="flex flex-col items-center gap1">
+        <NavLink to="/contact" className="flex flex-col items-center gap-1">
           <p>CONTACT</p>
           <hr className=" w-2/4 border-none h-[1.5px] bg-gray-600 hidden" />
         </NavLink>
       </ul>
-      <div className="flex items-center gap-16">
+      <div className="flex items-center gap-6">
         <Search className="text-gray-500" size={24} />
 
         <div className="group relative">
@@ -45,7 +47,43 @@ const Navbar = () => {
         </div>
         <Link to="/cart" className="relative">
           <ShoppingCart className="text-gray-500" size={24} />
+          <p className=" absolute right-[-5px] bottom-[-4px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
+            5
+          </p>
         </Link>
+        <Menu
+          onClick={() => setVisible(true)}
+          className="cursor-pointer sm:hidden"
+        />
+        <div
+          className={`absolute  top-0 right-0  bg-white transition-all ${
+            visible ? "w-full" : "w-0"
+          } overflow-y-auto z-50 `}
+        >
+          <div className=" flex flex-col text-gray-600">
+            <div
+              onClick={() => setVisible(false)}
+              className="flex items-center gap24 p-3 cursor-pointer"
+            >
+              <X className="rotate-180" />
+              <p>Back</p>
+            </div>
+            
+              <NavLink className="py-2 pl-6 border" to="/">
+                Home
+              </NavLink>
+              <NavLink className="py-2 pl-6 border" to="/collection">
+                COLLECTION
+              </NavLink>
+              <NavLink className="py-2 pl-6 border" to="/about">
+                ABOUT
+              </NavLink>
+              <NavLink className="py-2 pl-6 border" to="/contact">
+                CONTACT
+              </NavLink>
+            
+          </div>
+        </div>
       </div>
     </div>
   );
